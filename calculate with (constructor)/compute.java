c03d4535475d9a1b48d965d562c3ff4cb1cb6ed3
Jiya <59332847+giamnesia@users.noreply.github.com>
2021-06-04 18:16:43 +0800

    Create compute.java

---
+++ calculate with (constructor)/compute.java
@@ -0,0 +1,27 @@
+
+public class compute {
+
+    public static double add, subtraction, multiply, divide;
+    public static double num1, num2;
+    public static void add (double setNum1, double setNum2){
+        num1=setNum1;
+        num2=setNum2;
+        add= num1+num2;
+    }
+    public static void subtraction (double setNum1, double setNum2){
+        num1=setNum1;
+        num2=setNum2;
+        subtraction= num1-num2;
+    }
+    public static void multiply (double setNum1, double setNum2){
+        num1=setNum1;
+        num2=setNum2;
+        multiply =num1*num2;
+    }
+    public static void divide (double setNum1, double setNum2){
+        num1=setNum1;
+        num2=setNum2;
+        divide =num1/num2;
+    }
+
+}
